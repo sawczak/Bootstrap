822d3b89124f2bc1be54d33e59cdaa0bcd6cfe1a
Izabela Sawczak <izabela.ziemba@gmail.com>
2019-03-17 10:38:17 +0100

    strona - zakończenie 1/2

--- warsztaty/js/main.js
+++ warsztaty/js/main.js
@@ -1,1 +1,10 @@
-$('carousel').carousel();
+const nav = $('#nav');
+
+
+$(window).on('scroll', function() {
+    if($(window).scrollTop() > 50) {
+        nav.addClass('scroll');
+    } else {
+        nav.removeClass('scroll');
+    }
+});
